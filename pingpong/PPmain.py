08fee567ac38754b0a5b203b6b63955f3b7883d9
DanielRCaponius <146859076+DanielRCaponius@users.noreply.github.com>
2023-10-10 18:47:13 +0300

    Add files via upload
    Ball and collision.

--- pingpong/PPmain.py
+++ pingpong/PPmain.py
@@ -33,57 +33,66 @@ class GameSprite(sprite.Sprite):
 
 class player(GameSprite):
     def __init__(self,png,sx,sy,x,y,v,nplayer):
-        super().__init__()
+        super().__init__(png,sx,sy,x,y,v)
         self.nplayer = nplayer
         
-    
     def controls(self):
         keys_pressed = key.get_pressed()
 
         match self.nplayer:
             case 1:
-                if keys_pressed[K_d]:
-                    if self.rect.x > 620:
-                        self.rect.x = 620
+                if keys_pressed[K_w]:
+                    if self.rect.y < 0:
+                        self.rect.y = 0
                     else:
-                        self.rect.x += self.v
+                        self.rect.y -= self.v
 
-                if keys_pressed[K_a]:
-                    if self.rect.x < 0:
-                        self.rect.x == 0
+                if keys_pressed[K_s]:
+                    if self.rect.y > 450:
+                        self.rect.y = 450
                     else:
-                        self.rect.x -= self.v
+                        self.rect.y += self.v
+                    
             case 2:
-                if keys_pressed[K_UP]:
-                    if self.rect.x > 620:
-                        self.rect.x = 620
-                    else:
-                        self.rect.x += self.v
-
                 if keys_pressed[K_DOWN]:
-                    if self.rect.x < 0:
-                        self.rect.x == 0
+                    if self.rect.y > 450:
+                        self.rect.y = 450
                     else:
-                        self.rect.x -= self.v
+                        self.rect.y += self.v
+                               
+                if keys_pressed[K_UP]:
+                    if self.rect.y < 0:
+                        self.rect.y = 0
+                    else:
+                        self.rect.y -= self.v
 
 class ball(GameSprite):
+    def __init__(self,png,sx,sy,x,y,v,v_x,v_y):
+        super().__init__(png,sx,sy,x,y,v)
+        self.v_x = v_x
+        self.v_y = v_y
+    
     def update(self):
-        self.rect.y += self.v
-        self.rect.x += self.v
+        if self.rect.y < 0 or self.rect.y > 480:
+            self.v_y *= -1
+            
+        if self.rect.x < -20 or self.rect.x > 700:
+            self.v_x *= -1
         
-        match self.rect.y:
-            case 0, yres:
-                self.rect.y *= -1
-        
-        match self.rect.x:
-            case -20:
-                win = True
-                window.blit(p1win,(xres/2,yres/2))
-            case 720:
-                win = True
-                window.blit(p2win,(xres/2,yres/2))
+        self.rect.x += self.v_x
+        self.rect.y += self.v_y
                 
-        
+
+    '''    win = True
+        window.blit(p1win,(xres/2,yres/2))
+        case 720:
+        win = True
+        window.blit(p2win,(xres/2,yres/2))'''
+                
+p1 = player('p1.png',20,50,20,250,5,1)    
+p2 = player('p2.png',20,50,660,250,5,2)
+
+b = ball('b.png',25,25,350,250,0,3,3)
 
 while game:
     for e in event.get():
@@ -91,7 +100,20 @@ while game:
             game = False
 
     if win != True:
-    
+        
+        window.fill((255,242,212))
+        
+        if sprite.collide_rect(p1,b) or sprite.collide_rect(p2,b):
+            b.v_x *= -1
+        
+        p1.controls()
+        p2.controls()
+        b.update()
+        
+        p1.reset()
+        p2.reset()
+        b.reset()
+        
         display.update()
   
     display.update()
